3d33a620c9decdab50ce93268ff2a8a667bfefe3
Jerremy Stroman <jerremystroman@gmail.com>
2022-05-07 12:37:24 -0700

    Data shape changes

--- db/schema/schema.sql
+++ db/schema/schema.sql
@@ -6,15 +6,16 @@
 DROP TABLE IF EXISTS trail;
 
 CREATE TABLE trail (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(256),
-  city VARCHAR(128) NULL DEFAULT NULL,
-  description TEXT,
-  length NUMERIC,
-  elevation NUMERIC,
-  lat NUMERIC,
-  lng NUMERIC,
-  google_url TEXT DEFAULT NULL
+  id                  SERIAL PRIMARY KEY,
+  name                VARCHAR(256),
+  city                VARCHAR(128) NULL DEFAULT NULL,
+  short_description   VARCHAR(256),
+  description         TEXT,
+  length              NUMERIC,
+  elevation           NUMERIC,
+  lat                 NUMERIC,
+  lng                 NUMERIC,
+  google_url          TEXT DEFAULT NULL
 );
 
 -- ---
@@ -25,12 +26,31 @@ CREATE TABLE trail (
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  email TEXT NOT NULL,
-  username VARCHAR(80),
-  profile_image TEXT DEFAULT NULL,
-  bio TEXT DEFAULT NULL
+  id               SERIAL PRIMARY KEY,
+  email            TEXT NOT NULL,
+  username         VARCHAR(80),
+  profile_image    TEXT DEFAULT NULL,
+  bio              TEXT DEFAULT NULL
 );
+
+-- ---
+-- Table 'user_activity'
+--
+-- ---
+
+DROP TABLE IF EXISTS user_activity;
+
+CREATE TABLE user_activity (
+  id                      SERIAL PRIMARY KEY,
+  user_id                 INTEGER NOT NULL,
+  trail_id                INTEGER NOT NULL,
+  times_visited           INTEGER NOT NULL DEFAULT 1,
+  FOREIGN KEY (user_id)   REFERENCES users,
+  FOREIGN KEY (trail_id)  REFERENCES trail
+);
+
+CREATE INDEX ON user_activity (user_id);
+CREATE INDEX ON user_activity (trail_id);
 
 -- ---
 -- Table 'friends'
@@ -40,10 +60,13 @@ CREATE TABLE users (
 DROP TABLE IF EXISTS friends;
 
 CREATE TABLE friends (
-  id SERIAL PRIMARY KEY,
-  user_id INTEGER NOT NULL,
-  friend_id INTEGER NULL DEFAULT NULL,
-  timestamp DATE DEFAULT NOW()
+  id                      SERIAL PRIMARY KEY,
+  user_id                 INTEGER NOT NULL,
+  friend_id               INTEGER NOT NULL,
+  status                  VARCHAR(16),
+  timestamp               DATE DEFAULT NOW(),
+  FOREIGN KEY (user_id)   REFERENCES users,
+  FOREIGN KEY (friend_id) REFERENCES users
 );
 
 CREATE INDEX ON friends (user_id);
@@ -57,17 +80,18 @@ CREATE INDEX ON friends (friend_id);
 DROP TABLE IF EXISTS photos;
 
 CREATE TABLE photos (
-  id SERIAL PRIMARY KEY,
-  trail_id INTEGER,
-  user_id INTEGER,
-  url TEXT,
-  upvotes INTEGER,
-  timestamp DATE DEFAULT NOW(),
-  FOREIGN KEY (trail_id) REFERENCES trail,
-  FOREIGN KEY (user_id) REFERENCES users
+  id                      SERIAL PRIMARY KEY,
+  trail_id                INTEGER,
+  user_id                 INTEGER,
+  url                     TEXT,
+  score                   INTEGER,
+  timestamp               DATE DEFAULT NOW(),
+  FOREIGN KEY (trail_id)  REFERENCES trail,
+  FOREIGN KEY (user_id)   REFERENCES users
 );
 
 CREATE INDEX ON photos (trail_id);
+CREATE INDEX ON photos (user_id);
 
 -- ---
 -- Table 'Ratings'
@@ -77,16 +101,16 @@ CREATE INDEX ON photos (trail_id);
 DROP TABLE IF EXISTS ratings;
 
 CREATE TABLE ratings (
-  id SERIAL PRIMARY KEY,
-  trail_id INTEGER NOT NULL,
-  user_id INTEGER NOT NULL,
-  difficulty INTEGER NULL DEFAULT NULL,
-  nature INTEGER NULL DEFAULT NULL,
-  beauty INTEGER NULL DEFAULT NULL,
-  stars INTEGER NULL DEFAULT NULL,
-  timestamp DATE DEFAULT NOW(),
-  FOREIGN KEY (trail_id) REFERENCES trail,
-  FOREIGN KEY (user_id) REFERENCES users
+  id                      SERIAL PRIMARY KEY,
+  trail_id                INTEGER NOT NULL,
+  user_id                 INTEGER NOT NULL,
+  difficulty              INTEGER NULL DEFAULT NULL,
+  nature                  INTEGER NULL DEFAULT NULL,
+  beauty                  INTEGER NULL DEFAULT NULL,
+  stars                   INTEGER NULL DEFAULT NULL,
+  timestamp               DATE DEFAULT NOW(),
+  FOREIGN KEY (trail_id)  REFERENCES trail,
+  FOREIGN KEY (user_id)   REFERENCES users
 );
 
 CREATE INDEX ON ratings (trail_id);
@@ -99,15 +123,15 @@ CREATE INDEX ON ratings (trail_id);
 DROP TABLE IF EXISTS comments;
 
 CREATE TABLE comments (
-  id SERIAL PRIMARY KEY,
-  trail_id INTEGER NOT NULL,
-  user_id INTEGER NOT NULL,
-  comment_body TEXT,
-  helpfulness INTEGER NOT NULL DEFAULT 0,
-  reported BOOL DEFAULT false,
-  timestamp DATE DEFAULT NOW(),
-  FOREIGN KEY (trail_id) REFERENCES trail,
-  FOREIGN KEY (user_id) REFERENCES users
+  id                      SERIAL PRIMARY KEY,
+  trail_id                INTEGER NOT NULL,
+  user_id                 INTEGER NOT NULL,
+  body                    TEXT,
+  helpfulness             INTEGER NOT NULL DEFAULT 0,
+  reported                BOOL DEFAULT false,
+  timestamp               DATE DEFAULT NOW(),
+  FOREIGN KEY (trail_id)  REFERENCES trail,
+  FOREIGN KEY (user_id)   REFERENCES users
 );
 
 CREATE INDEX ON comments (trail_id);
