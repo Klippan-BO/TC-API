1a2ec7a22c841f2e053873d490d3c5eef9600137
Jerremy Stroman <jerremystroman@gmail.com>
2022-05-07 11:11:34 -0700

    Seed data

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const router = require('./routes');
 
 const port = process.env.PORT;
 const app = express();
+
+app.use(router);
 
 app.listen(port);
 console.log('[TC-API] Running on port:', port);
---
+++ routes/trails.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+
+router.get('/:trailId', (req, res))
+
+module.exports = router;
