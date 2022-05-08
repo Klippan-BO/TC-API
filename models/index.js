01659b5aae0759c0ee8223494e791efd59411a74
Jerremy Stroman <jerremystroman@gmail.com>
2022-05-07 22:06:34 -0700

    Trail by ID route

--- models/index.js
+++ models/index.js
@@ -1,8 +1,51 @@
 const db = require('../db');
 
+/*
+  trail1: {
+    name: 'Sunny trail',
+    photos: [
+      'http://www.trailstompers.com/uploads/2/6/8/6/2686411/9693769.jpg',
+      'http://www.trailstompers.com/uploads/2/6/8/6/2686411/5328225.jpg?327',
+      'http://www.trailstompers.com/uploads/2/6/8/6/2686411/2847590_orig.jpg',
+      'http://www.trailstompers.com/uploads/2/6/8/6/2686411/326294_orig.jpg',
+      'http://www.trailstompers.com/uploads/2/6/8/6/2686411/4025080_orig.jpg',
+      'http://www.trailstompers.com/uploads/2/6/8/6/2686411/6829184_orig.jpg',
+    ],
+    description: 'this is a great place for family and friends!',
+    ratings: {
+      average: 4.5,
+      beauty: 4,
+      nature: 4.5,
+      difficulty: 1.2,
+    },
+    comments: [
+      {
+        username: 'Justin',
+        body: 'every half mile and got to see the ocean at the top!',
+        timestamp: Date.now(),
+      },
+      {
+        username: 'Patrick',
+        body: 'This trail is amazing!!! We saw bees, trees, whales, and snails!',
+        timestamp: Date.now(),
+      },
+    ],
+  },
+*/
+
 module.exports = {
   getTrailById: (trailId) => {
-    const query = 'SELECT * FROM trail WHERE id = $1';
+    const query = `
+      SELECT name, description, length,
+        (SELECT json_agg(comment)
+        FROM (
+          SELECT body, username
+          FROM comments
+          WHERE comments.trail_id = t.id
+          ) AS comment
+        ) AS comments
+      FROM trail AS t
+      WHERE t.id = $1`;
     return db.query(query, [trailId])
       .then(({ rows }) => {
         console.log('[model] trail:', rows[0]);
