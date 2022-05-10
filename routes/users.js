68386d5a6ab94b4d2a5bcc460195151cbbb57f86
Jerremy Stroman <jerremystroman@gmail.com>
2022-05-10 14:23:13 -0700

    Documention session handler

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,17 @@
 const router = require('express').Router();
 const { users } = require('../models');
 
+routes.get('/me', (req, res) => {
+  // Gets user profile using session ID
+  console.log('[routes] request to get current users full user profile');
+  const session = req.cookies['trail-comp'];
+  if (!session) {
+    res.status(404).send('user not logged in!');
+  } else {
+    users.getFullUserBySession(session)
+  }
+});
+
 router.get('/:userId', (req, res) => {
   console.log('[routes] get user from users model');
   users.getUserProfileById(req.params.userId)
@@ -13,22 +24,22 @@ router.get('/:userId', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  const cookie = req.cookies['trail-comp'];
+  const session = req.cookies['trail-comp'];
   const user = req.body;
-  console.log('[routes] login request from user:', user, cookie);
-  if (cookie) {
+  console.log('[routes] login request from user:', user, session);
+  if (session) {
     res.send('User logged in');
   } else {
     users.login(user)
       .then(([userId, newCookie]) => {
-        res.cookie(newCookie).send(userId);
+        // After login -> set new cookie named "trail-comp" to session ID recorded in DB
+        res.cookie('trail-comp', newCookie).send(userId);
       })
       .catch((err) => {
         console.log('[routes] Issue logging in user:', err);
       });
   }
 });
-
 // users.login (user) => [userId, cookie]
 
 module.exports = router;
