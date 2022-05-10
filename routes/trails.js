91a04a4dab1b866fadd04339d89c0c03523e218c
Jerremy Stroman <jerremystroman@gmail.com>
2022-05-09 20:48:37 -0700

    Post request for trails within boundary added and documentaion on README. Need to add error boundary for bad coordinates

--- routes/trails.js
+++ routes/trails.js
@@ -2,10 +2,22 @@ const router = require('express').Router();
 const { trails } = require('../models');
 
 router.get('/:trailId', (req, res) => {
-  console.log('[trails] get trail from trails model');
+  console.log('[routes] get trail from trails model');
   trails.getTrailById(req.params.trailId)
     .then((trail) => {
       res.send(trail);
+    });
+});
+
+router.post('/map', (req, res) => {
+  const coords = req.body;
+  trails.getTrailsInBounds(coords)
+    .then((trailList) => {
+      console.log('[routes] gettings trails in-bounds:', coords);
+      res.send(trailList);
+    })
+    .catch((err) => {
+      console.log('[routes] issue getting trails in bounds:', err);
     });
 });
 
