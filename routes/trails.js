a4ea2324925e50c22d3c317d8c519810c292045a
Jerremy Stroman <jerremystroman@gmail.com>
2022-05-09 21:32:12 -0700

    Changed POST /trails/map to GET /trails/map to comply with REST

--- routes/trails.js
+++ routes/trails.js
@@ -1,23 +1,23 @@
 const router = require('express').Router();
 const { trails } = require('../models');
 
+router.get('/map', (req, res) => {
+  const coords = req.query;
+  console.log('[routes] gettings trails in-bounds:', coords);
+  trails.getTrailsInBounds(coords)
+    .then((trailList) => {
+      res.send(trailList);
+    })
+    .catch((err) => {
+      console.log('[routes] issue getting trails in bounds:', err);
+    });
+});
+
 router.get('/:trailId', (req, res) => {
   console.log('[routes] get trail from trails model');
   trails.getTrailById(req.params.trailId)
     .then((trail) => {
       res.send(trail);
-    });
-});
-
-router.post('/map', (req, res) => {
-  const coords = req.body;
-  trails.getTrailsInBounds(coords)
-    .then((trailList) => {
-      console.log('[routes] gettings trails in-bounds:', coords);
-      res.send(trailList);
-    })
-    .catch((err) => {
-      console.log('[routes] issue getting trails in bounds:', err);
     });
 });
 
