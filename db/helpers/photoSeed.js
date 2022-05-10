42536b979b36a9e6c685616b4eae350681cf9948
Jerremy Stroman <jerremystroman@gmail.com>
2022-05-10 13:28:15 -0700

    Updated photos in db. Included seed file

--- db/helpers/photoSeed.js
+++ db/helpers/photoSeed.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-path-concat */
 require('dotenv').config();
 const fs = require('fs');
@@ -5,13 +6,25 @@ const fs = require('fs');
 const dir = `${__dirname}/unsplash.json`;
 console.log(dir);
 
-fs.readFile(dir, (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    const parsed = JSON.parse(data);
-    parsed.forEach((e) => {
-      console.log(e);
-    });
-  }
-});
+const replacer = (key, value) => (value === null ? '' : value);
+
+// fs.readFile(dir, (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     const parsed = JSON.parse(data);
+//     const header = ['id', ...Object.keys(parsed[0])];
+//     const csv = [
+//       header.join(','),
+//       ...parsed.map((e, i) => header.map((field) => (
+//         JSON.stringify(i, e[field], replacer)
+//       )).join(',')),
+//     ].join('/r/n');
+//     console.log(csv);
+//     fs.writeFile(`${__dirname}/newphotos.csv`, csv, (err) => {
+//       if (err) {
+//         console.log(err);
+//       }
+//     });
+//   }
+// });
