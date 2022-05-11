85be8a8a176806691e0ca2aa1fe899e9e3c5b1bf
RphPandan <pascal9057@gmail.com>
2022-05-11 14:30:00 -0700

    added a partial route for post needs access to user id to be sent in from front end

--- models/photos.js
+++ models/photos.js
@@ -1,7 +1,9 @@
+/* eslint-disable camelcase */
 const db = require('../db');
 
 module.exports = {
   upvotePhoto: (id) => {
+    console.log(id, 'in the query')
     const query = `
       UPDATE photos
       SET score = score + 1
@@ -9,7 +11,25 @@ module.exports = {
     `;
     return db.query(query)
       .catch((err) => {
-        console.log('[model] Error upvoting photo', err);
+        // console.log('[model] Error upvoting photo', err);
       });
+  },
+  insertPhoto: (photo) => {
+    console.log(photo, 'in the query');
+    const query = `
+      INSERT INTO photos (user_id, trail_id, username, url, thumb)
+      VALUES ($1, $2, $3, $4, $5);
+    `;
+    const {
+      user_id, trail_id, username, url, thumb,
+    } = photo;
+    const photoValues = [user_id, trail_id, username, url, thumb];
+
+    console.log(query, photoValues);
+    // return db.query(query, photoValues)
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+    return 'hello from here';
   },
 };
--- routes/photos.js
+++ routes/photos.js
@@ -2,12 +2,22 @@ const router = require('express').Router();
 const photos = require('../models/photos');
 
 router.put('/:id/upvote', (req, res) => {
-  console.log('[routes] upvoting photo:', req.params.id);
+  console.log('[routes] upvoting photo:', req.params);
   photos.upvotePhoto(req.params.id)
     .then(res.send(204))
     .catch((err) => {
       console.log('[routes] issue upvoting photo:', err);
     });
+});
+
+router.get('/test', (req, res) => {
+  console.log('im here in test');
+  res.send('hello World');
+});
+
+router.post('/', (req, res) => {
+  photos.insertPhoto(req.body);
+  res.send('hello from photos post');
 });
 
 module.exports = router;
