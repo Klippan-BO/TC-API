df0f21b5316e48c36ec4f0bab117c4d779446d16
Jerremy Stroman <jerremystroman@gmail.com>
2022-05-12 11:19:59 -0700

    Friendship details

--- models/users.js
+++ models/users.js
@@ -146,7 +146,7 @@ module.exports.getAuthorizedUserProfile = (userId) => {
     ) AS trails,
     (SELECT json_agg(fnds)
     FROM (
-      SELECT f.id, f.username, f.profile_image, uf.status
+      SELECT uf.id AS friendship, f.id AS friend_id, f.username, f.profile_image, uf.status
       FROM friends uf JOIN users f
       ON (uf.user_id = u.id AND uf.friend_id = f.id)
       OR (uf.friend_id = u.id AND uf.user_id = f.id)
