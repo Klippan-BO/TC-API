22064d402698c443ecbfefa02313b2f7624a394f
Jerremy Stroman <jerremystroman@gmail.com>
2022-05-12 10:12:29 -0700

    Documentation

--- models/photos.js
+++ models/photos.js
@@ -11,28 +11,33 @@ module.exports = {
     `;
     return db.query(query)
       .catch((err) => {
-        // console.log('[model] Error upvoting photo', err);
+        console.log('[model] Error upvoting photo', err);
+        throw err;
       });
   },
   insertPhoto: (photo) => {
     console.log(photo, 'in the query');
     const query = `
-      INSERT INTO photos (user_id, trail_id, username, url, thumb)
-      VALUES ($1, $2, $3, $4, $5);
-    `;
+      INSERT INTO photos (user_id, trail_id, url, thumb, username)
+      SELECT $1, $2, $3, $4, u.username
+      FROM users u
+      WHERE u.id = $1
+      RETURNING *`;
     const {
-      user_id, trail_id, username, url, thumb,
+      user_id, trail_id, url, thumb,
     } = photo;
-    const photoValues = [user_id, trail_id, username, url, thumb];
-
-    console.log(query, photoValues);
-    return db.query(query, photoValues);
-    // .then((result) => {
-    //   console.log(result);
-    // })
-    // .catch((err) => {
-    //   console.log(err);
-    // });
+    const photoValues = [user_id, trail_id, url, thumb];
+    console.log('[model] posting photo:', query, photoValues);
+    return db.query(query, photoValues)
+      .then(({ rows }) => {
+        const newPhoto = rows[0];
+        console.log('[model] new photo posted:', newPhoto);
+        return newPhoto;
+      })
+      .catch((err) => {
+        console.log(err);
+        throw err;
+      });
   },
 
   getPhotos: (trail_id) => {
